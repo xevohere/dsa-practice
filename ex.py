1c34d803c0701c7abc90d0e4803339de059b3394
xevohere <alokgupta5695@gmail.com>
2025-12-29 10:31:11 +0530

    Initial Commit

---
+++ ex.py
@@ -0,0 +1,26 @@
+
+def binary_search(nums,left,right,t):
+    '''m = (left+right)//2
+    if t==nums[m]:
+        return m
+    elif t<nums[m]:
+        return binary_search(nums, left, m-1,t)
+    else:
+        return binary_search(nums, m+1, right, t)
+    '''
+    while left<=right:
+        m=(left+right)//2
+        if nums[m]==t:
+            return m
+        elif nums[m]<t:
+            left = m+1
+        else:
+            right = m-1
+    return -1
+        
+    
+nums = [10,20,30,40,50,60,70]
+n = len(nums)
+t=60
+pos = binary_search(nums, 0, n, t)
+print(pos)
