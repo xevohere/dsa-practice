6ec6c4ea28ba073cae6c30cd93fc0b672dfec1fc
xevohere <alokgupta5695@gmail.com>
2025-12-29 14:21:06 +0530

    Lower Bound

---
+++ bs_lower_bound.py
@@ -0,0 +1,17 @@
+def lower_bound(nums, low, high, target):
+    ans = high-low+1
+    
+    while low<high:
+        mid=(low+high)//2
+        
+        if nums[mid]>=target:
+            ans=mid
+            high = mid-1
+            
+        else:
+            low = mid+1
+    return ans 
+
+nums=[101,102,103,104,105,106,107,110, 115,119]
+p=lower_bound(nums, 0, 9, 114)
+print(p)
