61d269045bb14f47e1773e959b3eba6fa01f410d
xevohere <alokgupta5695@gmail.com>
2026-01-07 23:20:06 +0530

    updated

---
+++ bs_foor_ceil_in_sorted_array.py
@@ -0,0 +1,33 @@
+def lower_bound(nums, low, high, target):
+    ans = high-low+1
+    
+    while low<high:
+        mid=(low+high)//2
+        
+        if nums[mid]>=target:
+            ans=mid
+            high = mid-1
+            
+        else:
+            low = mid+1
+    return ans 
+
+
+def upper_bound(nums, low, high, target):
+    ans = high-low+1
+    
+    while low<high:
+        mid=(low+high)//2
+        
+        if nums[mid]>target:
+            ans=mid
+            high = mid-1
+            
+        else:
+            low = mid+1
+    return ans 
+
+nums=[101,102,103,104,104,106,107,110,115,119]
+p=lower_bound(nums, 0, 9, 104)
+q=upper_bound(nums,0,9,104)
+print(p-1, q)
